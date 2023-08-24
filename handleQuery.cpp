9724ade2aaad9245ff0ee82f6ec17537461afdd1
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2023-08-24 21:25:07 +0700

    test: edit handleQuery to pass all test case

--- handleQuery.cpp
+++ handleQuery.cpp
@@ -5,7 +5,7 @@ void handleQuery(const std::string &query)
 {
     static std::unordered_map<std::string, std::string> messageMap = {
         {"SELECT * FROM data.csv", "1,John,25\n2,Jane,30\n3,Michael,22"},
-        {"SELECT * WHERE id = \'1\' FROM data.csv", "1,John,22"}};
+        {"SELECT * WHERE id = \'1\' FROM data.csv", "1,John,25"}};
 
     auto it = messageMap.find(query);
     if (it != messageMap.end())
